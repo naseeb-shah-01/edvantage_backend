67aa60f4c5b49c9fa0daf77a63a722da64810ed3
naseeb-shah-01 <165763875+naseeb-shah-01@users.noreply.github.com>
2026-02-12 14:29:18 +0530

    Update CORS middleware to disallow credentials
    Changed allow_credentials from True to False in CORS middleware.

--- app/middleware/cors.py
+++ app/middleware/cors.py
@@ -2,17 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 origins = [
-    "http://localhost:3000"
-    ,
-    "https://www.edvantage.org.in",    "https://edvantage.org.in"
+    "http://localhost:3000",
+    "https://edvantage.org.in",
+    "https://www.edvantage.org.in",
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
